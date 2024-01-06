1d22baeeccb50e28ba623f7b06698eb486b76ec6
Mrinmoy09 <mrinmoy0908@gmail.com>
2024-01-06 22:59:44 +0600

    second commit

--- index.php
+++ index.php
@@ -3,44 +3,64 @@
     $food = "Pizza and Burger"; // global scope
     // var_dump($food); 
 
-    $quantity = 3;
-    $price = 4.99;
+   
+    $x= 20;
+    $y = 10;
+
+    // Arithmetic Operators - '+' , '-' , '/' , '*' , '**', '%';
+    $add = $x+ $y;
+    $substraction = $x-$y;
+    $divide = $x / $y;
+    $multiplication = $x * $y;
+    $modulas = $x % $y;
+    $exponentiation = $x ** $y;
+
+    
+    
+    echo"add - {$add} <br>";
+    echo"substraction - {$substraction} <br>";
+    echo"divide - {$divide} <br>";
+    echo"multiplication - {$multiplication} <br>";
+    echo"modulas - {$modulas} <br>";
+    echo"exponentiation - {$exponentiation}<br>";
+    
+    
 
 
-    $total;
-    $total = $price*$quantity;
+    //Assignment Operators - '=',  '+=' , "-=" , '*=' , '/='
 
-    echo"You have ordered {$quantity}X Pizzas <br>";
-    echo"Total cost is \${$total}";
+    $x  -= $y;
+    echo"{$x} <br>";
+
 
     //You can get the data type of ant object by using the var_dump() function
 
     //A variable declared outside a function has a GLOBAL SCOPE and can only be accessed outside a function
     
-    function info(){
-        $userName = "John Doe"; // Local scope
-        $age = 24; 
-        $gpa = 3.12;
-        $price = 9.12;
+    // function info(){
+    //     $userName = "John Doe"; // Local scope
+    //     $age = 24; 
+    //     $gpa = 3.12;
+    //     $price = 9.12;
         
-        // using global keyword can make a variable global and we can acess it outside the funtion
-        global $name, $food;
-        echo "Hello My name is {$name} and I like {$food}<br>";
-        //A variable declared within a function has a LOCAL SCOPE and can only be accessed within that function
-        echo"Hello This is {$userName} and his age is {$age} and His GPA is {$gpa}<br>";
-        echo"Price of the pizza is \${$price}<br>"; // \$ sign will print a dollar sign 
-    }
+    //     // using global keyword can make a variable global and we can acess it outside the funtion
+    //     global $name, $food;
+    //     echo "Hello My name is {$name} and I like {$food}<br>";
+    //     //A variable declared within a function has a LOCAL SCOPE and can only be accessed within that function
+    //     echo"Hello This is {$userName} and his age is {$age} and His GPA is {$gpa}<br>";
+    //     echo"Price of the pizza is \${$price}<br>"; // \$ sign will print a dollar sign 
+    // }
 
-    info();
+    // info();
    
-    function myTest() {
-        static $x = 0;
-        echo $x;
-        $x++;
-      }
+    // function myTest() {
+    //     static $x = 0;
+    //     echo $x;
+    //     $x++;
+    //   }
 
-    myTest();
-    myTest();
+    // myTest();
+    // myTest();
     
     
 ?>
