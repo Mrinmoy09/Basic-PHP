778b18ff03a7fee434aa146724d4cb483c3ab97f
Mrinmoy09 <mrinmoy0908@gmail.com>
2024-01-07 21:52:02 +0600

    php conditions

---
+++ ifelse.php
@@ -0,0 +1,18 @@
+<?php 
+    $t = date("H");
+    if ($t < "20") {
+      echo "Have a good day!";
+    } else {
+      echo "Have a good night!";
+    }
+
+    // if (condition) {
+    //     code to be executed if this condition is true;
+    //   } elseif (condition) {
+    //     code to be executed if first condition is false and this condition is true;
+    //   } else {
+    //     code to be executed if all conditions are false;
+    // }
+
+    //You can have if statements inside if statements, this is called nested if statements
+?>
